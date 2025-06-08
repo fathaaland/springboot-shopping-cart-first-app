1afa7d492bc5c36384d2195c2004f858e2ce40bf
David Dobrovolny <david.dobrovolny@plus4u.net>
2025-06-08 11:34:59 +0200

    general class definition

--- src/main/java/com/dailycodework/dreamshops/model/Product.java
+++ src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -1,4 +1,31 @@
 package com.dailycodework.dreamshops.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Product {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private BigDecimal price;
+    private int inventory;
+
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
 }
