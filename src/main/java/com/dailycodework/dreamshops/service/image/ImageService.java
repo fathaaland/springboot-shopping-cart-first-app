0eb440b7bcfeedf943ec09fb900b86664a050caf
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-06-12 17:46:56 +0200

    implement ii into service, without changing so far

--- src/main/java/com/dailycodework/dreamshops/service/image/ImageService.java
+++ src/main/java/com/dailycodework/dreamshops/service/image/ImageService.java
@@ -1,4 +1,27 @@
 package com.dailycodework.dreamshops.service.image;
 
-public class ImageService {
+import com.dailycodework.dreamshops.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageService implements IImageService {
+
+    @Override
+    public Image getImageById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteImageById(Long id) {
+
+    }
+
+    @Override
+    public Image saveImage(MultipartFile file, Long productId) {
+        return null;
+    }
+
+    @Override
+    public void updateImage(MultipartFile file, Long imageId) {
+
+    }
 }
