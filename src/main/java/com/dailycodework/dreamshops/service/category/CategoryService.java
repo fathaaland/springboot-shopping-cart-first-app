07cffcdc13fefd8419e9bf4c1955ba52e5049484
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-06-12 16:45:25 +0200

    implementing methods from interface

--- src/main/java/com/dailycodework/dreamshops/service/category/CategoryService.java
+++ src/main/java/com/dailycodework/dreamshops/service/category/CategoryService.java
@@ -1,4 +1,40 @@
 package com.dailycodework.dreamshops.service.category;
 
-public class CategoryService {
+import com.dailycodework.dreamshops.model.Category;
+import com.dailycodework.dreamshops.repository.ProductRepository;
+
+import java.util.List;
+
+public class CategoryService implements ICategoryService {
+
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return List.of();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+
+    }
 }
