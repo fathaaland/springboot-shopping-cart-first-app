15953ee703e3d0c19bbfb7b83f6da87daf96879d
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-06-16 17:11:55 +0200

    create addCategory

--- src/main/java/com/dailycodework/dreamshops/controller/CategoryController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/CategoryController.java
@@ -1,16 +1,18 @@
 package com.dailycodework.dreamshops.controller;
 
+import com.dailycodework.dreamshops.exceptions.AlreadyExistsException;
 import com.dailycodework.dreamshops.model.Category;
 import com.dailycodework.dreamshops.response.ApiResponse;
 import com.dailycodework.dreamshops.service.category.ICategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RequiredArgsConstructor
@@ -30,6 +32,17 @@ public class CategoryController {
               return ResponseEntity.status(INTERNAL_SERVER_ERROR)
                      .body(new ApiResponse("Failed to fetch categories: " + e.getMessage(), null));
        }
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+        try {
+            Category thecategory = categoryService.addCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Category added successfully", thecategory));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
     }
 
 
