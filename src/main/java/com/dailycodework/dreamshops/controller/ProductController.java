6f2bdea44e291406796e542d4250d42693119b7c
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-06-18 21:40:50 +0200

    new ProductController, getAllProducts

--- src/main/java/com/dailycodework/dreamshops/controller/ProductController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/ProductController.java
@@ -1,4 +1,34 @@
 package com.dailycodework.dreamshops.controller;
 
+
+import com.dailycodework.dreamshops.model.Product;
+import com.dailycodework.dreamshops.response.ApiResponse;
+import com.dailycodework.dreamshops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+
+    private final IProductService productService;
+
+    @GetMapping("/products/all")
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        try{
+            List<Product> products = productService.getAllProducts();
+            return ResponseEntity.ok(new ApiResponse("Products fetched successfully", products));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new ApiResponse("Failed to fetch products: " + e.getMessage(), null));
+        }
+    }
+
+
 }
