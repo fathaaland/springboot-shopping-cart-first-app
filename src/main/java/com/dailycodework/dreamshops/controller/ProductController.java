1310c351f815b4c291bb70e074fe88ef35697a45
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-06-18 21:49:56 +0200

    new ProductController, addProduct

--- src/main/java/com/dailycodework/dreamshops/controller/ProductController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.dailycodework.dreamshops.controller;
 
 
 import com.dailycodework.dreamshops.model.Product;
+import com.dailycodework.dreamshops.request.AddProductRequest;
 import com.dailycodework.dreamshops.response.ApiResponse;
 import com.dailycodework.dreamshops.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +35,17 @@ public class ProductController {
             return ResponseEntity.ok(new ApiResponse("Product fetched successfully", product));
         } catch (Exception e) {
             return ResponseEntity.status(404).body(new ApiResponse("Product not found: " + e.getMessage(), null));
+        }
+    }
+
+
+    @PostMapping("/product/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest product) {
+        try {
+            Product theProduct = productService.addProduct(product);
+            return ResponseEntity.ok(new ApiResponse("Product added successfully", theProduct));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new ApiResponse("Failed to add product: " + e.getMessage(), null));
         }
     }
 
